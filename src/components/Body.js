f7e3c9142ec3598aaebcc386c7f7daa215394fdd
ChrisB007 <chrisbitoy@gmail.com>
2021-08-15 10:13:53 -0400

    footed and body update

---
+++ src/components/Body.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div className="bg-gradient-to-r from-gray-900 to-black text-white flex justify-center items-center pt-12 flex-col">
+      This is the body
+    </div>
+  );
+}
+
+export default Body;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Body from "./Body";
+import Footer from "./Footer";
 import Hero from "./Hero";
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Body />
+      <Footer />
     </div>
   );
 }
