4cfb61abadd30db7a6b7209b19b9a226425fc154
ChrisB007 <chrisbitoy@gmail.com>
2021-08-18 11:08:21 -0400

    updated cors route

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const authAction = () => async (dispatch) => {
   try {
-    const getUser = await axios.get("/auth/google/api/current_user");
+    const getUser = await axios.get(
+      "https://calm-beyond-30005.herokuapp.com/auth/google/api/current_user"
+    );
     dispatch({ type: "FETCH_USER", payload: getUser.data });
   } catch (error) {
     console.log(error);
